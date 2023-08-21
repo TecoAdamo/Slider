bd94b02890bc8c3ed70ca319e1ab8b26b9987721
Mateus Adamo <tecoadamo2003@gmail.com>
2023-08-21 12:46:42 -0300

    Aplicando slider

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React , { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import Slider from "@react-native-community/slider";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+
+constructor(props){
+  super(props);
+  this.state= {
+    valor: 1000
+   }
+}
+
+  render(){
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Slider
+          style={{ width: 200 }}
+          minimumValue={0}
+          maximumValue={100}
+          onValueChange={(valor) => this.setState({valor})}
+          value={this.state.valor}
+          minimumTrackTintColor="green"
+          maximumTrackTintColor="#8b0000"
+        />
+        <Text style={{textAlign: 'center', fontSize: 30}}> 
+          Saldo - R$: {this.state.valor.toFixed(2)} 
+        </Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    flex:1,
+    marginTop: 150
   },
-});
+  text:{
+    marginTop: 90,
+  }
+})
+
+export default App;
